62b47287ab7ba1471e283c2e0ebab832b33684e3
lokesh <lokesh@gmail.com>
2024-03-31 15:04:25 +0530

    recent changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AWS_project_using_Terraform
 aws using terraform to create infrasture in aws
+
--- provider.tf
+++ provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.11.0"
+      version = "5.43.0"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
 }
